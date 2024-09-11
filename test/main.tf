5c6d12de5ec7cc564bb247a17ebdb5b72733b159
sanathlokanath1991 <sanath.lokanath@collaberadigital.com>
2024-09-11 17:59:45 +0530

    Initial terraform code

--- test/main.tf
+++ test/main.tf
@@ -2,201 +2,88 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
 
-  tags = {
-    Name = "Main VPC"
+  cidr = "10.0.0.0/16"
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 18.0"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.24"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    instance_types = ["t3.large"]
+    min_size       = 3
+    max_size       = 6
+    desired_size   = 3
   }
 }
 
-resource "aws_subnet" "public_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+module "rds" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "~> 5.0"
 
-  tags = {
-    Name = "Public Subnet 1"
+  identifier = "my-rds-instance"
+
+  engine            = "mysql"
+  engine_version    = "8.0"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+  multi_az          = true
+
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+
+  subnet_ids = module.vpc.private_subnets
+}
+
+module "dynamodb" {
+  source = "terraform-aws-modules/dynamodb-table/aws"
+
+  name           = "my-dynamodb-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attributes = [
+    {
+      name = "id"
+      type = "N"
+    }
+  ]
+}
+
+module "s3" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-app-logs"
+  acl    = "log-delivery-write"
+}
+
+module "s3_website" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-static-website"
+  acl    = "public-read"
+
+  website = {
+    index_document = "index.html"
+    error_document = "error.html"
   }
 }
 
-resource "aws_subnet" "public_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.1/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "Public Subnet 2"
-  }
-}
-
-resource "aws_subnet" "public_3" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.2/24"
-  availability_zone = "us-east-1c"
-
-  tags = {
-    Name = "Public Subnet 3"
-  }
-}
-
-resource "aws_subnet" "private_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "Private Subnet 1"
-  }
-}
-
-resource "aws_subnet" "private_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.1/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "Private Subnet 2"
-  }
-}
-
-resource "aws_subnet" "private_3" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.2/24"
-  availability_zone = "us-east-1c"
-
-  tags = {
-    Name = "Private Subnet 3"
-  }
-}
-
-resource "aws_subnet" "data_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "Data Subnet 1"
-  }
-}
-
-resource "aws_subnet" "data_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.1/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "Data Subnet 2"
-  }
-}
-
-resource "aws_subnet" "data_3" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.2/24"
-  availability_zone = "us-east-1c"
-
-  tags = {
-    Name = "Data Subnet 3"
-  }
-}
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "Main IGW"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "Public Route Table"
-  }
-}
-
-resource "aws_route_table_association" "public_1" {
-  subnet_id      = aws_subnet.public_1.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public_2" {
-  subnet_id      = aws_subnet.public_2.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public_3" {
-  subnet_id      = aws_subnet.public_3.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_security_group" "allow_https" {
-  name        = "Allow HTTPS"
-  description = "Allow HTTPS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "Allow HTTPS"
-  }
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "Allow SSH"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["YOUR_TRUSTED_IP/32"]
-  }
-
-  tags = {
-    Name = "Allow SSH"
-  }
-}
-
-resource "aws_security_group" "allow_internal" {
-  name        = "Allow Internal Traffic"
-  description = "Allow internal traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = [aws_vpc.main.cidr_block]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = [aws_vpc.main.cidr_block]
-  }
-
-  tags = {
-    Name = "Allow Internal Traffic"
-  }
-}
+# Security groups, load balancing, monitoring, CI/CD, secrets, access, etc.
+# Additional resources can be added as needed
